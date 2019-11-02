796e89c4470310ae313872170c5e577cae4a8799
Pramod Patil <pramod.ndroid@gmail.com>
2019-11-02 15:21:59 +0530

    Initial commit

---
+++ app/src/main/java/com/mywork/dailytransaction/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mywork.dailytransaction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
